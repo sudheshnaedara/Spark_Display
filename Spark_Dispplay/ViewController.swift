238dbefcefbed0486a3eabbab28f9b6a7fa5f105
Edara Sudeshna Devi <edara.devi@spark.co.nz>
2018-11-08 12:34:56 +0530

    Initial Commit

---
+++ Spark_Dispplay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spark_Dispplay
+//
+//  Created by SparkMac on 08/11/18.
+//  Copyright © 2018 Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Spark_DispplayTests/Spark_DispplayTests.swift
@@ -0,0 +1,36 @@
+//
+//  Spark_DispplayTests.swift
+//  Spark_DispplayTests
+//
+//  Created by SparkMac on 08/11/18.
+//  Copyright © 2018 Ibrahim. All rights reserved.
+//
+
+import XCTest
+@testable import Spark_Dispplay
+
+class Spark_DispplayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
